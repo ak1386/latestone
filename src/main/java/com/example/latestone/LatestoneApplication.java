415cb1b6907a39d5054a6e350edbd98b74d0b848
ak1386 <nileshnaik1386@gmail.com>
2020-02-27 10:11:24 +0530

    codeRefactor

---
+++ src/main/java/com/example/latestone/LatestoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.latestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/latestone/LatestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
